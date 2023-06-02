4f480264ed761367f0ae04854522105b388dbfb0
YaniryFlorez <ymflorezrivera@gmail.com>
2023-06-02 14:59:15 -0500

    subir

---
+++ src/main/java/com/example/demo/PwU1P4YfrApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwU1P4YfrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwU1P4YfrApplication.class, args);
+	}
+
+}
